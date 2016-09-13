1b45e989298ab5e253f138299a008f7da99e924e
corydave <corydave@users.noreply.github.com>
2016-09-13 14:40:02 -0400

    Create P02-MadLibs.java

---
+++ P02-MadLibs.java
@@ -0,0 +1,14 @@
+package madLibs;
+
+// Dave Ghidiu
+
+// This is a funny, simple Mad Libs program
+
+public class MadLibs {
+
+	public static void main(String[] args) {
+		// Insert cool code here
+
+	}
+
+}

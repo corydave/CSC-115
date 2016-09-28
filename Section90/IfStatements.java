99a88d9edd516b78845482e3913055bbb6111483
corydave <corydave@users.noreply.github.com>
2016-09-27 21:09:43 -0400

    Create IfStatements.java

---
+++ Section90/IfStatements.java
@@ -0,0 +1,127 @@
+public class IfStatements {
+
+	public static void main(String[] args) {
+		/*
+		 * IF statements are the heart and soul of programming.
+		 */
+
+		// int num = 5;
+		// if (num > 5) {
+		// System.out.println("your number is bigger than or equal to 5");
+		// }
+		//
+		// if (num > 3) {
+		// System.out.println("Your number is bigger than 3");
+		// }
+
+		// IF ELSE statements
+
+		// int num2 = 3;
+		//
+		// if (num2 > 6) {
+		// System.out.println("YEAH!");
+		// } else {
+		// System.out.println("NOPE!");
+		// }
+
+		// IF ELSE IF
+
+		// int num3 = 18;
+		//
+		// if (num3 > 15) {
+		// System.out.println("A");
+		// } else if (num3 > 12) {
+		// System.out.println("B");
+		// } else {
+		// System.out.println("C");
+		// }
+
+		// if (num3 > 15) {
+		// System.out.println("A");
+		// }
+		//
+		// if (num3 > 12) {
+		// System.out.println("B");
+		// }
+
+		// TWO COMMON PITFALLS OF IF STATEMENTS:
+
+		// int num4 = 10;
+
+		// if (num4 > 20); { // ROUGE SEMICOLON!!!!!!!
+		// System.out.println("YES!");
+		// }
+
+		// if (num4 > 50) // NO BRACKETS = 1 line of code only
+		// System.out.println("YES");
+		// System.out.println("YES x2");
+		//
+
+		// Logical Comparison Thingies
+
+		// int num5 = 10;
+		// int num6 = 10;
+		//
+		// if (num5 == num6) { // == means "are they equal?"
+		// System.out.println("EQUAL!");
+		// }
+		// THIS ONLY WORKS FOR PRIMITIVE DATA TYPES
+		// int boolean char short long float byte double
+
+//		String a = "Happy";
+//		String b = "Happy";
+//
+//		if (a.equals(b)) { // Use .equals for OBJECTS and Strings
+//			System.out.println("Happy Birthday");
+//		}
+
+		// Logical Operators
+		
+//		int momsAge = 50;
+//		int myAge = 16;
+//
+//		if ((momsmyAge > 17) && (myAge > 17)) {
+//			System.out.println("Blair Witch!");
+//		}
+		
+
+		// Short Circuit Evaluation		
+		
+//		int num7 = 7;
+//		int num8 = 8;
+//		
+//		if (num7 != num8) {
+//			System.out.println("NOT Equal");
+//		}
+		
+		
+//		boolean flag = true;
+//		
+//		if (!flag) {
+//			System.out.println("FALSE!");
+//		} else {
+//			System.out.println("TRUE");
+//		}
+		
+		// int num9 = 100;
+		// int num10 = 25;
+		//
+		// if (!(num9 - num10 > 50)) {
+		// System.out.println("A");
+		// }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}

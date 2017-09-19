6b066f31948803972f618c6fc6b649a79ecb6002
corydave <corydave@users.noreply.github.com>
2017-09-19 12:09:19 -0400

    Update 2017-09-14-ScannerDemo.java

--- FALL2017/2017-09-14-ScannerDemo.java
+++ FALL2017/2017-09-14-ScannerDemo.java
@@ -4,23 +4,49 @@ public class ScannerDemo {
   public static void main (String[] args) {
   
    Scanner scanner = new Scanner(System.in);
+   
+   // Declare three variables
    int age;
    double gpa;
    String name;
    
 
 
+   // Prompt the user for a value to put into 'age'
    System.out.print("AGE: ");
+   
+   // Take what the user typed and store it in 'age'
    age = scanner.nextInt();
    
+   
+    
+    
+
+   // Prompt the user for a value to put into 'gpa'    
    System.out.print("GPA: ");
+    
+   // Take what the user typed and store it in 'gpa'
    gpa = scanner.nextDouble();
 
+    
+    
+   
+   // Need this line IF we "scanned-in" numbers before "scanning-in" a String
    scanner.nextLine();
-      
+    
+    
+    
+    
+   // Prompt the user for a value to put into 'name'
    System.out.print("NAME: ");
+  
+   // Take what the user typed and store it in 'name' 
    name = scanner.nextLine();
       
+    
+    
+    
+   // Output the variables mixed with String literals (CONCATENATION)
    System.out.print(age + ", " + gpa + ", and " + name);
    
    

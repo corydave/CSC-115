0dc69eeebc6b3fe6211ac259547d285af9ac0cc8
corydave <corydave@users.noreply.github.com>
2017-09-26 23:41:36 -0400

    Create 2017-09-21-Escapes.java

---
+++ FALL2017/2017-09-21-Escapes.java
@@ -0,0 +1,25 @@
+/* Aaron Sullivan 
+ * Experiment with escape sequences!!
+ * 9/21/2017
+ */
+ public class Escape {
+   public static void main(String[] args) {
+      System.out.print("Hi.\n");
+      System.out.print("Aga\nin.\n");
+      System.out.println("Hello,\nagain.");
+      System.out.println("Aaron says, \"code well.\"");
+      System.out.println("Look! \\ A backslash!");
+      System.out.println("\tThis is indented");
+   }
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 

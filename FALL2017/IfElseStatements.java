fe2d29c1f68457cb6e17a9ef19a6642171d3b52a
corydave <corydave@users.noreply.github.com>
2017-10-12 12:26:01 -0400

    Create IfElseStatements.java

---
+++ FALL2017/IfElseStatements.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+public class IfElseStatements {
+  public static void main (String args[]) {
+  
+    int grade = 20;
+    
+//     if (grade >= 90) {
+//       System.out.println("A");
+//     } else if (grade >= 80) {
+//         System.out.println("B");
+//     } else if (grade >= 70) {
+//         System.out.println("C");
+//     } else if (grade >= 65) {
+//         System.out.println("D");
+//     } else { // This is the default - if none of the other IFs fire, this one will!
+//         System.out.println("F");
+//     }      
+
+
+
+
+//     if (grade >= 90) {
+//       System.out.println("A");
+//     } else if ((grade >= 80) && (grade < 90)) {
+//       System.out.println("B");
+//     } else if ((grade >= 70) && (grade < 80)) {
+//       System.out.println("C");
+//     } else if ((grade >= 65) && (grade < 70)) {
+//       System.out.println("D");
+//     } else {
+//       System.out.println("F");
+//     }    
+    
+    
+    
+// THIS IS WRONG!!!!! vv
+//     if (grade > 70 && grade < 80)
+
+    Scanner scanner = new Scanner(System.in);
+      System.out.print("What is your name? ");
+      String name = scanner.nextLine();
+      
+      System.out.println("Your name is " + name);
+      
+      if (!name.equals("Larry Bird")) {
+        System.out.println("WINNER winner WINNER!");
+      }
+
+    
+//     System.out.println("Enter a number: ");
+//     int num1 = scanner.nextInt();
+    
+//     if (num1 != 33) {
+//       System.out.println("LOSER");
+//     }
+    
+    
+//     if (!(num1 == 33)) {
+//       System.out.println("Winner winner!");
+//     }
+    
+    
+//     if ((num1 == 33) || (num1 == 22) || (num1 == 23)) {
+//       System.out.println("WINNER!");
+//     }
+
+//     if (((num1 > 10) && (num1 < 30)) || ((num1 > 100) && (num1 < 200))) {
+//       System.out.println("Oh jeez I don't know anything about anything!");
+//       int winningNumber = num1;
+//       System.out.println("winningNumber = " + winningNumber);      
+//     }
+// 
+//     int winningNumber = 66;
+// 
+//     System.out.println("winningNumber = " + winningNumber);          
+    
+
+
+
+    
+    
+    
+    
+    
+    
+
+  }
+}

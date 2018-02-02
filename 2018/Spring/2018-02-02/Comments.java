babb33e72e28fac92902ef1b396c9d66b73d5715
corydave <corydave@users.noreply.github.com>
2018-02-02 10:57:23 -0500

    Create Comments.java

---
+++ 2018/Spring/2018-02-02/Comments.java
@@ -0,0 +1,87 @@
+// Written by Dave Ghidiu
+// This program is a demonstration of commenting
+
+public class Comments {
+    public static void main (String[] args) {
+        
+        // anything you type is NOT read by the computer.
+        
+        /*
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        */
+        System.out.println("Hello World!");
+        
+        System.out.println("Hello World!");
+        
+        
+        System.out.println("Hello World!");
+        
+        
+        // We use comments for:
+        //   - Adding YOUR name to the program
+        //   - Adding a brief description of the program
+        
+        // DEBUGGING
+        //   - If there is code that doesn't work (...yet)
+        //   - If there are a bunch of lines of code that don't work
+        //     and you want to isolate them
+        
+        // GIVE CREDIT
+        //   - If you use someone else's code, use comments
+        //     to credit them and include the place you got it from
+        //
+        //   -> Shamelessly stolen from: https://github.com/potatolondon/search/commit
+        
+        // HOW DO I MAKE COMMENTS?
+        //   I use block comments I am NOT changing the contents of
+        //   the block of code (or text) such as an outline of a program or 
+        //   the name/purpose of the program
+        
+        /* This program is made by Dave Ghidiu
+        
+           It is an approximation of the linear congruential method
+           and looks at the number of milliseconds that have elapsed since
+           the epoch and uses that as a seed for pseudorandom number generation
+           
+        */
+        
+        // THIS IS CONTROL + /
+        
+        // System.out.println("This is a comment");
+        // System.out.println("This is a comment");
+        System.out.println("This is a comment");
+        // System.out.println("This is a comment");
+        System.out.println("This is a comment");
+        // System.out.println("This is a comment");
+        // System.out.println("This is a comment");
+        System.out.println("This is a comment");
+        
+        
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

16e009149c28d856e3f14adfbc6159e26172ea53
corydave <corydave@users.noreply.github.com>
2018-02-02 10:58:25 -0500

    Create HelloWorld.java

---
+++ 2018/Spring/2018-01-31/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+  public static void main (String[] args) {
+
+    for (int i  = 0; i < 3; i++) {
+        for (int j = 0; j < i; j++) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+    
+    System.out.println("The quick brown fox jumped over the lazy dog and then he jumped over the moon and the moon is made of cheese and also you can't eat that cheese #trustMePlease");
+    
+    
+    System.out.println("Hello W\t\torld\n");
+    System.out.println();
+    System.out.println("THIS WILL OUTPUT THEN GO TO A NEW LINE");
+    System.out.println("Hello World");
+    
+    
+  }
+}

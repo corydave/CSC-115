0d0fbba8adec9660298fa40ea95f5c77c9e63d96
corydave <corydave@users.noreply.github.com>
2018-02-02 10:57:47 -0500

    Create Variables.java

---
+++ 2018/Spring/2018-02-02/Variables.java
@@ -0,0 +1,51 @@
+public class Variables {
+    public static void main (String[] args) {
+        
+        // There are EIGHT "primitive data types"
+        // We will use only 3 or 4 this semester
+        
+        //  int   f    =    18;
+        //  type
+        //        variable name
+        //             assignment
+        //                  value
+        
+        
+        
+        int a = 4; // integer (whole number, negative or positive);
+        int a2 = 5;
+        int a3; // a3 has been DECLARED but it has NOT been ASSIGNED
+        // System.out.println(a3); // WON'T WORK!!!!!
+        
+        System.out.println(a + a2);
+        
+        double b = 3.14; // decimal (again, positive or negative);
+        System.out.println(a + b);
+        
+        boolean c = true; // either 'true' or 'false'
+        char d = 'l'; // character - it's really just ONE letter/symbol/etc.
+        char e = '4'; // a character representation of 4, not actually 4
+        System.out.println(e + a2); // output: 57
+        
+        /*
+         long - a realllllllly big int
+         short - a reallllllly small int
+         float - a decimal, but less precise than double
+         byte - uh.... I dunno
+        
+        */
+        
+        
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
+

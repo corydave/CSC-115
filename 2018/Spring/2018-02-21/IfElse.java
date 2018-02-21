b0518ab5c1a4e4638313f419e2f1e79f1602afd1
corydave <corydave@users.noreply.github.com>
2018-02-21 11:13:35 -0500

    Update IfElse.java

--- 2018/Spring/2018-02-21/IfElse.java
+++ 2018/Spring/2018-02-21/IfElse.java
@@ -1,1 +1,35 @@
 
+
+public class IfElse {
+    public static void main (String[] args) {
+        
+        
+        int num1 = 10;
+        int num2 = 2;
+        int num3 = 20;
+        
+        if (num2 > num1) {
+            System.out.println("num2 is greater");
+        } else {
+            System.out.println("num2 is NOT greater");
+        }
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

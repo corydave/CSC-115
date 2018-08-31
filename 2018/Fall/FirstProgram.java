51bb2183430cc67c4e8d1e8847d42b7210d74604
corydave <corydave@users.noreply.github.com>
2018-08-30 21:17:38 -0400

    Create FirstProgram.java

---
+++ 2018/Fall/FirstProgram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FirstProgram {
+  public static void main (String[] args) {
+  
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter an integer:");
+    int a = scanner.nextInt();
+    System.out.println("Your number is: " + a);
+  
+  }
+}

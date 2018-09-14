2c12ded3088ce1c2f0d9632869876bf67bbf9575
corydave <corydave@users.noreply.github.com>
2018-09-14 10:45:30 -0400

    Update BlowYerMind.java

--- 2018/Fall/BlowYerMind.java
+++ 2018/Fall/BlowYerMind.java
@@ -1,1 +1,152 @@
-// A demo on things that are weird in Java (Output ONLY)
+// <Insert your name here>
+// CSC-103
+// This program is designed to blow yer mind.
+
+public class BlowYerMind {
+
+	
+	
+	public static void main(String[] args) {
+
+		System.out.println("HI!");
+		// THIS WON'T DO ANYTHING BAD - it's called a _comment_
+		System.out.println(2 + 3);
+		System.out.println("2 + 3");
+		
+		
+		System.out.println("Hi" + "folks");
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("Hi" + " folks");
+		System.out.println("Hi " + "folks");
+		System.out.println("Hi" + " " + "folks");
+		
+		System.out.print("Dave Ghidiu " + "plays kazoo");
+		System.out.println("Dave Kazoo?");
+		
+		System.out.println(2 + 3 + " is the answer to 2 + 3");
+		System.out.println("The answer to 2 + 3 is " + 2 + 3);
+		
+		System.out.print("The answer to 2 + 3 is ");
+		System.out.println(2 + 3);
+		System.out.println();
+		System.out.print("The answer to 2 + 3 is " + (2 + 3));
+		
+		System.out.println(); // WHITESPACE
+		
+		int age = 40;
+		
+		System.out.println(age);
+		System.out.println("age");
+		
+		System.out.println("My age is " + age);
+		System.out.println("My age in 2020 is " + age + 2); // 402
+		System.out.println("My age in 2020 is " + (age + 2)); // 42
+		
+		System.out.println();
+		
+		String name = "Dave";
+		
+		System.out.println(name); // Dave
+		
+//		int pi = 3.14;
+		
+		System.out.println(name + " is " + age + " years old.");
+		
+//		char ' = ";
+		System.out.println(name + " lives in a 'VAN' down by the RIVER");
+		System.out.println(name + " lives in a ''VAN'' down by the RIVER");
+		
+//		System.out.println(""");
+		
+		System.out.println(name + " lives in a \"VAN\" down by the RIVER");
+		// The backslash is called the "escape character"
+		
+		System.out.println("4\\5");
+		// The escape character is used to escape a backslash
+		
+		
+		System.out.println("     Indent");
+		System.out.println("\t\t\t\tIndent");
+		// The escape character can be used with "t" to make a tab   \t
+		
+		System.out.println("\n\n\n\nHmmmmmmmm....");
+		// The escape character can be us3d with "n" to make a new line \n
+		
+		System.out.println(name + "\nis awe\nsome......");
+		
+		// FYI: \a USED to make computers beep. But it was annoying so they got rid of it.
+		
+		
+		////////////////////////////////////////////////////////////////////////////////////
+		
+		// Let's do some MATH~!!!!!! :)
+		
+		System.out.println();
+		System.out.println(10/2); // 5
+		System.out.println("11/2 is " + (11/2)); // 5
+		System.out.println("11.0/2 is " + (11.0/2)); // 5.5
+		
+		System.out.println(1/10); // 1? 10? 0? 
+		// TRUNCATE?!?!?!?!?!
+		
+		System.out.println(1.0/10); // 1? 10? 0?
+		
+		System.out.println(15/2); // 7? 7.5? 8?
+		System.out.println(15/2.0); // 7? 7.5? 8?
+		
+		double pi = 3.14;
+		
+		System.out.println(pi);
+		
+		double eggcellent = 12;
+		
+		System.out.println(eggcellent); // 12.0
+		
+		System.out.println(12/5); // 2 
+		System.out.println(eggcellent/5); // 2.4
+		
+		System.out.println(9/11);
+		
+		
+		
+		
+		
+		
+		
+		
+//		int quote = ";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();		
+		
+		
+		
+		
+
+	}
+
+}

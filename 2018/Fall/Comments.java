d1debedf78ca2981d4ed006492ebc22f47fda04e
corydave <corydave@users.noreply.github.com>
2018-09-17 10:59:22 -0400

    Create Comments.java

---
+++ 2018/Fall/Comments.java
@@ -0,0 +1,55 @@
+public class Variables {
+    public static void main (String[] args) {
+        
+        int age; // Primitive Data Type
+        // 'int' holds integers (positive and negative)
+
+        int numberOfStudents = 17;
+        int passengersOnAirplane = 146;
+        int snakesOnAPlane = 1028;
+        
+
+        
+        double dennisAge; // Primitive Data Type
+        // 'double' holds decimals
+
+        double depositAmount = 12.50;
+        double twoHundredMeterDashTime = 23.42;
+        double average;
+        
+        
+        
+        char choice; // Primitive Data Type
+        // 'char' holds a character (letter, @, $, /, ', A - Any single keystroke)
+        // yesOrNo, middleInitial 
+        
+        char yesOrNo = 'y';
+        char middleInitial = 'K';
+        
+        
+        boolean flag; // Primitive Data Type
+        // 'boolean' holds either TRUE or FALSE
+        
+        boolean debugFlag = true;
+        boolean output = false;
+
+        
+        String name; // Class
+        // 'String' IT HAS A FREAKING CAPITAL IN THE BEGINNING
+        
+        String firstName = "Dave";
+        String lastName = "Kazoo";
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+

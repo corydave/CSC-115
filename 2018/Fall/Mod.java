29a5d6b6f63cbf6d2689b19732120406e50a2588
corydave <corydave@users.noreply.github.com>
2018-09-24 10:49:46 -0400

    Create Mod.java

---
+++ 2018/Fall/Mod.java
@@ -0,0 +1,24 @@
+public class Mod {
+    public static void main (String[] args) {
+        
+        final double SALES_TAX = .075; // constant
+        final int NUMBER_OF_FINGERS = 4; // constant for Simpsons
+        
+        System.out.print(5/3); // 1
+        System.out.print(" r. ");
+        System.out.println(5%3); // 2 (because 2 is the remainder of 5/3)
+        
+        System.out.println();
+        
+        // c = 30; // This will cause an error because 'c' has not been DECLARED yet
+        
+        int a; // DECLARE a variable
+        a = 10; // ASSIGNING a variable
+        
+        // NOTE! You can (and often will) DECLARE and ASSIGN in one step!
+        int b = 20;
+        
+        System.out.println("The value of a is " + a);
+        
+    }
+}

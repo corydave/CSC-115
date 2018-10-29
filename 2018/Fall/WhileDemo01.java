ca357dfca7d01d7a32bbbb4a00523a122373d217
corydave <corydave@users.noreply.github.com>
2018-10-29 10:51:44 -0400

    Create WhileDemo01.java

---
+++ 2018/Fall/WhileDemo01.java
@@ -0,0 +1,26 @@
+public class WhileDemo01 {
+    public static void main (String[] args) {
+        
+        int num = 1;
+        
+        while (true) {
+            
+            System.out.println(num);
+            // num = num + 1;
+            num *= 13;
+            
+            // num++;
+            
+            
+        }
+        
+        // System.out.println("GAME OVER MAN!");
+        
+    }
+}
+
+
+
+
+
+

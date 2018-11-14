090fe7303852102b4a38a40e7a7c4f1d32f49c3a
corydave <corydave@users.noreply.github.com>
2018-11-14 11:07:01 -0500

    Create DoubleLoop.java

---
+++ 2018/Fall/DoubleLoop.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+import java.util.Random;
+
+// #splodeProof
+
+public class DoubleLoop {
+    public static void main (String[] args) {
+        
+
+        for (int i = 0; i < 5; i++) {
+            
+            for (int j = 0; j <=3; j++) {
+               System.out.print("*");
+            }
+            
+            System.out.print("-");
+            
+            for (int j = 0; j <=3; j++) {
+               System.out.print("*");
+            }            
+
+            System.out.println();
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println();
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter a seed:");
+        // int seed = scanner.nextInt();
+        
+        // Random random = new Random(seed);    
+
+
+
+
+
+
+        // for (int i = 7; i <= 21; i+=2) {
+            
+        //     int tempRandomNumber = random.nextInt(10) + 1;
+        //     System.out.println("Random number " + i + ": " + tempRandomNumber);
+
+        // }

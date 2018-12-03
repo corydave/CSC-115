bcdcb1f4baedfe73ddaa0f87285661b401bde559
corydave <corydave@users.noreply.github.com>
2018-12-03 10:47:32 -0500

    Finished the program

--- 2018/Fall/Password.java
+++ 2018/Fall/Password.java
@@ -8,20 +8,74 @@ public class Password {
         
         Scanner scanner = new Scanner(System.in);
         
-        // displayMenu();
+        System.out.println("Enter a choice: ");
+        System.out.println("  1. Display the rules");
+        System.out.println("  2. Validate a password");
+        System.out.println("  3. Generate a password");
+        System.out.println();
         
+        int choice = scanner.nextInt();
         
-        System.out.println("Enter a password to verify:");
-        String password = scanner.nextLine();
+        switch (choice) {
+            case 1: displayRules();
+                    break;
+            
+            case 2: scanner.nextLine();
+                    System.out.println("Enter a password to verify:");
+                    String password = scanner.nextLine();
         
-        if (verifyPassword(password) == true) {
-            System.out.println("Your password is good to use.");   
-        } else {
-            System.out.println("Your password is CRAP!");
-            System.out.println("Initiate 'splode sequence.");
+                    if (verifyPassword(password) == true) {
+                        System.out.println("Your password is good to use.");   
+                    } else {
+                        System.out.println("Your password is CRAP!");
+                        System.out.println("Initiate 'splode sequence.");
+                    }
+                    break;
+            
+            case 3: System.out.println("Your password is: " + generatePassword());
+                    break;
+            
+            default: System.out.println("ID 10 T issue.");
+
         }
+
+        System.out.println("\nGAME OVER MAN!!!!");
         
     }
+    
+    public static String generatePassword() {
+        
+        Random random = new Random();
+        
+        String pw = "";
+        
+        while (verifyPassword(pw) == false) {
+            pw += (char)(random.nextInt(90) + 33);
+            System.out.println(pw);
+        }
+
+        return pw;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     public static boolean verifyPassword(String pw) {
         
@@ -33,50 +87,42 @@ public class Password {
         int numberCounter = 0;
         int capsCounter = 0;
         int lowerCounter = 0;
-        
-        
+
         // Is there at least one NUMBER in the password?
+        // Are there at least two UPPERCASE letters?
+        // Are there at least two LOWERCASE letters?
+        // http://www.asciitable.com/
+        
         for (int i = 0; i < pw.length(); i++) {
             
             char temp = pw.charAt(i);
             int tempValue = (int)temp;
+            
             if (tempValue >= 48 && tempValue <= 57) {
                 numberCounter++;
             }
-            
-            
-            
+
+            if (tempValue >= 65 && tempValue <= 90) {
+                capsCounter++;
+            }
+
+            if (tempValue >= 97 && tempValue <= 122) {
+                lowerCounter++;
+            }            
+
         }
         
-        return true;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // Are there at least two UPPERCASE letters?
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        if (numberCounter >= 1 && capsCounter >= 2 && lowerCounter >= 2) {
+            return true;
+        } else {
+            return false;
+        }
+
         
     }
     
     
-    public static void displayMenu() {
+    public static void displayRules() {
         
         System.out.println("There needs to be:");
         System.out.println("  At least eight characters");

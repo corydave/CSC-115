1b5d95e61892e7eb4d062554ec0bbdfc3bb11200
corydave <corydave@users.noreply.github.com>
2019-01-25 10:48:10 -0500

    Create CommentsDemo.java

---
+++ 2019/Spring/CommentsDemo.java
@@ -0,0 +1,38 @@
+/*
+
+Dave Ghidiu
+
+Worked with Emma F on this code!
+
+This program shows how to use MOD to determine if numbers
+are even or odd
+
+*/
+
+
+public class CommentsDemo {
+    
+    public static void main (String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        int num = 6;
+        
+        // Tests the value of num to see if it's even or odd
+        if (num % 2 == 0) { 
+            
+        // Outputs the word 'EVEN'
+        System.out.println("EVEN!");
+            
+        } else {
+            
+        // Outputs the word 'ODD'
+        System.out.println("ODD!");
+        }
+        
+    }
+    
+}
+
+
+

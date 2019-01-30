2444df8f841449724405c253dec950719de2a42c
corydave <corydave@users.noreply.github.com>
2019-01-30 11:55:07 -0500

    Create BlowYerMind.java

---
+++ 2019/Spring/BlowYerMind.java
@@ -0,0 +1,57 @@
+public class BlowYerMind {
+    public static void main (String[] args) {
+        
+        
+        // CONCATENATE
+        System.out.println("Dave " + "Ghidiu" + " Ryan");
+        
+        // System.out.println("3 + 4");
+        // System.out.println(3 + 4);
+        // System.out.println(3 + 4 + " = 3 + 4");
+        
+        // System.out.println("3 + 4 = " + 3 + 4);
+        
+        System.out.println(3 + 4 + " = 3 + 4 = " + 3 + 4);
+        
+        System.out.println(3 + 4 + " = 3 + 4 = " + (3 + 4));
+        
+        System.out.println(3 - 4);
+        
+        // System.out.println("3 - 4 = " + 3 - 4); // ERROR!
+        System.out.println("3 - 4 = " + (3 - 4));
+        
+        
+        int difference = 3 - 4;
+        System.out.println("3 - 4 = " + difference);
+        
+        int num1;
+        int num2;
+        
+        // Have the user input num1 and num2 values
+        
+        // System.out.println("The difference is " + (num2-num1));
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

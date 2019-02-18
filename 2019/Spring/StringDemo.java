eaeab4ab15e2b96e5409fee575e0e5ba6bc43f17
corydave <corydave@users.noreply.github.com>
2019-02-18 10:51:44 -0500

    Create StringDemo.java

---
+++ 2019/Spring/StringDemo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class StringDemo {
+    public static void main (String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("DEMO OF STRINGS\n\n");
+        
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        
+        System.out.println("Welcome, " + name);
+        
+        System.out.println("I SAID HI, " + name.toUpperCase());
+        System.out.println("Name: " + name);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println("\n\nGAME OVER MAN!");
+        
+    }
+}

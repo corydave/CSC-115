1fbd1e05115752c9479442cd29844f86576527c5
corydave <corydave@users.noreply.github.com>
2019-03-25 10:47:38 -0400

    Update MethodDemo.java

--- 2019/Spring/MethodDemo.java
+++ 2019/Spring/MethodDemo.java
@@ -4,43 +4,73 @@ public class MethodDemo {
     public static void main (String[] args) {
         
         
-        int c = 10;
-        System.out.println("c = " + c);
-        changeValue(c);
-        System.out.println("c = " + c);
+        // int c = 10;
+        // System.out.println("c = " + c);
+
+        // changeValue(c);
+        // System.out.println("c = " + c);
+
+        // c = changeValue(c);
+        // System.out.println("c = " + c);        
+        
         
         Scanner scanner = new Scanner(System.in);
+        
         
         int a, b;
         System.out.println("Enter two numbers:");
         a = scanner.nextInt();
         b = scanner.nextInt();
         
-        // System.out.println("DEBUG: a = " + a + " b = " + b);
+        // String d, e;
+        // System.out.println("Enter two Strings:");
+        // d = scanner.nextLine();
+        // e = scanner.nextLine();
+
+        System.out.println("DEBUG: a = " + a + " b = " + b);
         
-        System.out.println("Enter a name:");
-        scanner.nextLine();
-        String name = scanner.nextLine();
+        // System.out.println("Enter a name:");
+        // scanner.nextLine();
+        // String name = scanner.nextLine();
         
         displayMenu();
+        int choice = scanner.nextInt();
         
-        System.out.println(add(a, b));
-        
-        // Method Overloading
-        System.out.println(mult(a, b));
-        System.out.println(mult(a));
-        System.out.println(mult(name));
-        
-        
-        if (b != 0) {
-            System.out.println(div(a, b));
-        } else {
-            System.out.println("Can't divide by zero!");
+        switch(choice) {
+            case 1:  System.out.println(add(a, b));
+                     break;
+            case 2:  System.out.println(sub(a, b));
+                     break;
+            case 3:  int answer = mult(a, b);
+                     System.out.println(answer);
+                     break;
+            case 4:  div(a, b);
+                    //  System.out.println(1.0*a / b);
+                     break;
+            default: System.out.println("YOU DIED!");
+                    //  return;
+                     break;
         }
         
-        System.out.println(sub(a, b));
         
-        System.out.println("GAME OVER MAN!");
+        
+        // System.out.println(add(a, b));
+        
+        // Method Overloading
+        // System.out.println(mult(a, b));
+        // System.out.println(mult(a));
+        // System.out.println(mult(name));
+        
+        
+        // if (b != 0) {
+        //     System.out.println(div(a, b));
+        // } else {
+        //     System.out.println("Can't divide by zero!");
+        // }
+        
+        // System.out.println(sub(a, b));
+        
+        System.out.println("\n\nGAME OVER MAN!");
         
         scanner.close();
         
@@ -59,7 +89,7 @@ public class MethodDemo {
    //Modifier        Type     Name    List
     public    static int      add     (int num1, int num2) {
         
-        System.out.println("I am preparing to add " + num1 + " and " + num2 +".");
+        // System.out.println("I am preparing to add " + num1 + " and " + num2 +".");
         int sum = num1 + num2;
         return sum;
         
@@ -75,9 +105,9 @@ public class MethodDemo {
 
     }
     
-    public static int mult(int a) {
+    public static double mult(int a) {
         
-        return (int)(Math.pow(a, a));
+        return Math.pow(a, a);
         
     }
     
@@ -85,31 +115,32 @@ public class MethodDemo {
         return "" + a + a;
     }
     
-    public static double div (int a, int b) {
+    public static void div (int a, int b) {
         
-        return 1.0*a / b;
-        
-    }
-    
-    public static int sub (int aa, int bb) {
-        
-        return aa - bb;
+        System.out.println(1.0*a / b);
         
     }
     
-    public static void changeValue(int c) {
+    public static int sub (int aasdfa, int bb) {
+        
+        return aasdfa - bb;
+        
+    }
+    
+    public static int changeValue(int c) {
         
         c = 20;
+        return c;
         
     }
     
     public static void displayMenu() {
     
-        System.out.println("1. add");
+        System.out.println("\n1. add");
         System.out.println("2. subtract");
         System.out.println("3. multiply");
         System.out.println("4. divide");
-        System.out.println("Please make a selection.");
+        System.out.println("Please make a selection:\n");
     
     }
     

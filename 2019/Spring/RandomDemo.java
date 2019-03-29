ae518d6868f56ec25048c2abce6238b7de9fb8bc
corydave <corydave@users.noreply.github.com>
2019-03-29 10:21:28 -0400

    Create RandomDemo.java

---
+++ 2019/Spring/RandomDemo.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main (String[] args) {
+        
+        // "Seeding" the random number generator
+        Random random = new Random(33);
+        
+        System.out.println("GENERATE A CAPITAL LETTER:");
+        
+        // Generate a random number between 0 and 26
+        
+        // Add 65 to that number so that we are looking
+        // at the capital letters
+        
+        // Cast it as a char
+        
+        System.out.println((char)(random.nextInt(26) + 65));
+        
+        
+    }
+}
+
+
+
+
+
+
+
+

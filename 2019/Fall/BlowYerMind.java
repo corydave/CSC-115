2f659ebefb0adce8e0f9e87bc48557425474dcee
corydave <corydave@users.noreply.github.com>
2019-09-18 11:55:52 -0400

    Create BlowYerMind.java

---
+++ 2019/Fall/BlowYerMind.java
@@ -0,0 +1,73 @@
+public class BlowYerMind {
+    public static void main (String[] args) {
+        
+        int age = 41;
+        System.out.println(age);
+        String actor = "Bill Paxton";
+        
+        System.out.println(2 + 3);
+        System.out.println(2 - 3);
+        System.out.println("2 + 3");
+        System.out.println(2 + 3 + " = 2 + 3");
+        System.out.println("2 + 3 = " + 2 + 3);
+        System.out.println(2 + 3 + " = 2 + 3 = " + 2 + 3);
+        System.out.println(2 + 3 + actor + 2 + 3);
+        
+        System.out.println("But..." + (2 - 3));
+        System.out.println((2 + 3) + " = 2 + 3");
+        
+        System.out.println(10/2);
+        System.out.println(10/3);
+        System.out.println(1.0*10/3);
+        System.out.println(10.0/3);
+        System.out.println(10/3.0);
+        System.out.println(10.0/3.0);
+        
+        System.out.println();
+        
+        System.out.println("I want sharks with frickin' laser beams.");
+        System.out.println("I want sharks with frickin' \"laser beams\".");
+        
+        System.out.println("How do I output the escape character?");
+        System.out.println("\\");
+        System.out.println("\\\"");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("This\n\tis\na \t\tnew\nline\n\t\t\t\t\tee cummings");
+        
+        
+        
+       
+       
+       
+       
+       
+        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        
+        System.out.println("GAME OVER MAN!!!!");
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
